9f826670ae9cc0585748c83026978a072948d038
RodolfoPeralta <rodolfoperalta1994@hotmail.com>
2025-01-20 15:29:49 -0300

    Some changes

--- src/services/FileService.js
+++ src/services/FileService.js
@@ -2,6 +2,18 @@ const fs = require('fs');
 
 class FileService {
 
+    static createFile(path) {
+
+    }
+
+    static readFile(path) {
+
+    }
+
+    static writeFile(path) {
+        
+    }
+
 }
 
 module.exports = FileService;
