f5a3577c082d93583090f480270aede9301c1cef
Rodolfo Peralta <rodolfoperalta1994@hotmail.com>
2025-02-07 08:01:15 -0300

    Pre Entrega 1

--- src/app.js
+++ src/app.js
@@ -1,31 +1,12 @@
 const express = require('express');
 const cartRouter = require('./routes/cartRouter');
 const productRouter = require('./routes/productRouter');
-const multer = require('multer');
-const path = require('path');
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
-// Multer
-const storageConfig = multer.diskStorage({
-    destination: (request, file, cb) => {
-        cb(null, path.resolve(__dirname, './uploads'));
-    },
-    filename: (request, file, cb) => {
-        const timestamp = Date.now();
-        const originalname = file.originalname;
-        const ext = path.extname(originalname);
-        cb(null, `${timestamp}-${originalname}`);
-    }
-});
-
-const upload = multer({storage: storageConfig});
-
-app.post('/upload', upload.single('file'), (request, reponse) => {});
 
 // Routes
 
